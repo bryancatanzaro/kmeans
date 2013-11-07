b0f09b5505edb77919c4c429b9055d178f61a077
Bryan Catanzaro <bcatanzaro@acm.org>
2013-11-07 15:38:40 -0800

    Simplify code.

--- labels.cu
+++ labels.cu
@@ -1,5 +1,4 @@
 #include "labels.h"
-#include <cublas_v2.h>
 
 namespace kmeans {
 namespace detail {
@@ -15,77 +14,44 @@ void labels_init() {
     }
 }
 
-
-template<> void calculate_distances<double>(int n, int d, int k,
-                                            thrust::device_vector<double>& data,
-                                            thrust::device_vector<double>& centroids,
-                                            thrust::device_vector<double>& data_dots,
-                                            thrust::device_vector<double>& centroid_dots,
-                                            thrust::device_vector<double>& pairwise_distances) {
-    detail::make_self_dots(k, d, centroids, centroid_dots);
-    detail::make_all_dots(n, k, data_dots, centroid_dots, pairwise_distances);
-    //||x-y||^2 = ||x||^2 + ||y||^2 - 2 x . y
-    //pairwise_distances has ||x||^2 + ||y||^2, so beta = 1
-    //The dgemm calculates x.y for all x and y, so alpha = -2.0
-    double alpha = -2.0;
-    double beta = 1.0;
-    //If the data were in standard column major order, we'd do a
-    //centroids * data ^ T
-    //But the data is in row major order, so we have to permute
-    //the arguments a little
-    cublasStatus_t stat =
-        cublasDgemm(detail::cublas_handle,
-                    CUBLAS_OP_T, CUBLAS_OP_N,
-                    n, k, d, &alpha,
-                    thrust::raw_pointer_cast(data.data()),
-                    d,//Has to be n or d
-                    thrust::raw_pointer_cast(centroids.data()),
-                    d,//Has to be k or d
-                    &beta,
-                    thrust::raw_pointer_cast(pairwise_distances.data()),
-                    n); //Has to be n or k
-    
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        std::cout << "Invalid Dgemm" << std::endl;
-        exit(1);
-    }
-
-}
-
-template<> void calculate_distances<float>(int n, int d, int k,
-                                           thrust::device_vector<float>& data,
-                                           thrust::device_vector<float>& centroids,
-                                           thrust::device_vector<float>& data_dots,
-                                           thrust::device_vector<float>& centroid_dots,
-                                           thrust::device_vector<float>& pairwise_distances) {
-    detail::make_self_dots(k, d, centroids, centroid_dots);
-    detail::make_all_dots(n, k, data_dots, centroid_dots, pairwise_distances);
-    //||x-y||^2 = ||x||^2 + ||y||^2 - 2 x . y
-    //pairwise_distances has ||x||^2 + ||y||^2, so beta = 1
-    //The dgemm calculates x.y for all x and y, so alpha = -2.0
-    float alpha = -2.0;
-    float beta = 1.0;
-    //If the data were in standard column major order, we'd do a
-    //centroids * data ^ T
-    //But the data is in row major order, so we have to permute
-    //the arguments a little
-    cublasStatus_t stat =
-        cublasSgemm(detail::cublas_handle,
-                    CUBLAS_OP_T, CUBLAS_OP_N,
-                    n, k, d, &alpha,
-                    thrust::raw_pointer_cast(data.data()),
-                    d,//Has to be n or d
-                    thrust::raw_pointer_cast(centroids.data()),
-                    d,//Has to be k or d
-                    &beta,
-                    thrust::raw_pointer_cast(pairwise_distances.data()),
-                    n); //Has to be n or k
-    
-    if (stat != CUBLAS_STATUS_SUCCESS) {
+void gemm(cublasOperation_t transa,
+          cublasOperation_t transb,
+          int m, int n, int k,
+          const float *alpha,
+          const float *A, int lda,
+          const float *B, int ldb,
+          const float *beta,
+          float *C, int ldc) {
+    cublasStatus_t status = cublasSgemm(cublas_handle, transa, transb,
+                                        m, n, k, alpha,
+                                        A, lda,
+                                        B, ldb,
+                                        beta,
+                                        C, ldc);
+    if (status != CUBLAS_STATUS_SUCCESS) {
         std::cout << "Invalid Sgemm" << std::endl;
         exit(1);
     }
+}
 
+void gemm(cublasOperation_t transa,
+                    cublasOperation_t transb,
+                    int m, int n, int k,
+                    const double *alpha,
+                    const double *A, int lda,
+                    const double *B, int ldb,
+                    const double *beta,
+                    double *C, int ldc) {
+    cublasStatus_t status = cublasDgemm(cublas_handle, transa, transb,
+                                        m, n, k, alpha,
+                                        A, lda,
+                                        B, ldb,
+                                        beta, 
+                                        C, ldc);
+    if (status != CUBLAS_STATUS_SUCCESS) {
+        std::cout << "Invalid Sgemm" << std::endl;
+        exit(1);
+    }
 }
 
 }
--- labels.h
+++ labels.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <thrust/device_vector.h>
 #include <cfloat>
+#include <cublas_v2.h>
 
 namespace kmeans {
 namespace detail {
@@ -78,27 +79,52 @@ void make_all_dots(int n, int k, thrust::device_vector<T>& data_dots,
                         thrust::raw_pointer_cast(dots.data()));
 }
 
+void gemm(cublasOperation_t transa,
+          cublasOperation_t transb,
+          int m, int n, int k,
+          const float *alpha,
+          const float *A, int lda,
+          const float *B, int ldb,
+          const float *beta,
+          float *C, int ldc);
+
+void gemm(cublasOperation_t transa,
+          cublasOperation_t transb,
+          int m, int n, int k,
+          const double *alpha,
+          const double *A, int lda,
+          const double *B, int ldb,
+          const double *beta,
+          double *C, int ldc);
+
 template<typename T>
 void calculate_distances(int n, int d, int k,
                          thrust::device_vector<T>& data,
                          thrust::device_vector<T>& centroids,
                          thrust::device_vector<T>& data_dots,
                          thrust::device_vector<T>& centroid_dots,
-                         thrust::device_vector<T>& pairwise_distances);
-
-template<> void calculate_distances<float>(int n, int d, int k,
-                                           thrust::device_vector<float>& data,
-                                           thrust::device_vector<float>& centroids,
-                                           thrust::device_vector<float>& data_dots,
-                                           thrust::device_vector<float>& centroid_dots,
-                                           thrust::device_vector<float>& pairwise_distances);
-
-template<> void calculate_distances<double>(int n, int d, int k,
-                                            thrust::device_vector<double>& data,
-                                            thrust::device_vector<double>& centroids,
-                                            thrust::device_vector<double>& data_dots,
-                                            thrust::device_vector<double>& centroid_dots,
-                                            thrust::device_vector<double>& pairwise_distances);
+                         thrust::device_vector<T>& pairwise_distances) {
+    detail::make_self_dots(k, d, centroids, centroid_dots);
+    detail::make_all_dots(n, k, data_dots, centroid_dots, pairwise_distances);
+    //||x-y||^2 = ||x||^2 + ||y||^2 - 2 x . y
+    //pairwise_distances has ||x||^2 + ||y||^2, so beta = 1
+    //The dgemm calculates x.y for all x and y, so alpha = -2.0
+    T alpha = -2.0;
+    T beta = 1.0;
+    //If the data were in standard column major order, we'd do a
+    //centroids * data ^ T
+    //But the data is in row major order, so we have to permute
+    //the arguments a little
+    gemm(CUBLAS_OP_T, CUBLAS_OP_N,
+         n, k, d, &alpha,
+         thrust::raw_pointer_cast(data.data()),
+         d,//Has to be n or d
+         thrust::raw_pointer_cast(centroids.data()),
+         d,//Has to be k or d
+         &beta,
+         thrust::raw_pointer_cast(pairwise_distances.data()),
+         n); //Has to be n or k
+}
 
 template<typename T>
 __global__ void make_new_labels(int n, int k, T* pairwise_distances,
